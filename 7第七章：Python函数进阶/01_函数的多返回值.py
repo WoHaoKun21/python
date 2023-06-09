5ea274974010fe1599911694224b262bee526268
石鹏飞\60991 <609914130@qq.com>
2023-06-09 10:27:33 +0800

    docs：函数多种传参结束——进入匿名函数

--- 7第七章：Python函数进阶/01_函数的多返回值.py
+++ 7第七章：Python函数进阶/01_函数的多返回值.py
@@ -1,3 +1,12 @@
 """
     Python函数进阶：函数多返回值
 """
+
+
+# 使用多个变量接受多个返回值
+def test_return():
+    return 1, "Hello", True
+
+
+x, y, z = test_return()  # 变量和值对应，就会一个个接受
+print(f"多个变量接受多个返回值的结果分别为：{x}-{y}-{z}")
